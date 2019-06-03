8f901572ba443007e5d9b070d305d3465e00afa0
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-03 17:30:27 -0500

    Pet1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyectoPets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
